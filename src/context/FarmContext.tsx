a137fc4e0827cee9fb48a4e39371cf7e37a55371
kagalaeggtrading-source <kagalaeggtrading@gmail.com>
2026-09-25 23:46:08 +0800

    Master Architecture framework deployment

--- src/context/FarmContext.tsx
+++ src/context/FarmContext.tsx
@@ -2742,7 +2742,7 @@ export const FarmProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
       {children}
     </FarmContext.Provider>
   );
-
+};
 
 export const useFarm = () => {
   const context = useContext(FarmContext);
